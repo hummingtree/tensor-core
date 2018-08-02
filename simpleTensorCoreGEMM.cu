ae212236b2ce2b21062f384e244a026b2a502aed
Jiqun Tu <jt2798@columbia.edu>
2018-08-02 09:38:24 -0700

    fix tab-space.

--- simpleTensorCoreGEMM.cu
+++ simpleTensorCoreGEMM.cu
@@ -5,23 +5,23 @@
 // Define some error checking macros.
 #define cudaErrCheck(stat) { cudaErrCheck_((stat), __FILE__, __LINE__); }
 void cudaErrCheck_(cudaError_t stat, const char *file, int line) {
-	if (stat != cudaSuccess) {
-		fprintf(stderr, "CUDA Error: %s %s %d\n", cudaGetErrorString(stat), file, line);
-	}
+  if (stat != cudaSuccess) {
+    fprintf(stderr, "CUDA Error: %s %s %d\n", cudaGetErrorString(stat), file, line);
+  }
 }
 
 #define cublasErrCheck(stat) { cublasErrCheck_((stat), __FILE__, __LINE__); }
 void cublasErrCheck_(cublasStatus_t stat, const char *file, int line) {
-	if (stat != CUBLAS_STATUS_SUCCESS) {
-		fprintf(stderr, "cuBLAS Error: %d %s %d\n", stat, file, line);
-	}
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    fprintf(stderr, "cuBLAS Error: %d %s %d\n", stat, file, line);
+  }
 }
 
 #define curandErrCheck(stat) { curandErrCheck_((stat), __FILE__, __LINE__); }
 void curandErrCheck_(curandStatus_t stat, const char *file, int line) {
-	if (stat != CURAND_STATUS_SUCCESS) {
-		fprintf(stderr, "cuRand Error: %d %s %d\n", stat, file, line);
-	}
+  if (stat != CURAND_STATUS_SUCCESS) {
+    fprintf(stderr, "cuRand Error: %d %s %d\n", stat, file, line);
+  }
 }
 
 
@@ -41,16 +41,16 @@ const int WMMA_K = 16;
 
 //template< class hf >
 //__forceinline __device__ void crop_tile(hf* to, hf* from,
-//													const int& col,
-//													const int& t_row, 				const int& t_col, 
-//													cosnt int& row_dim, 			const int& col_dim // column major
-//													const int& wmma_row_dim, 	const int& wmma_col_dim)
+//                          const int& col,
+//                          const int& t_row,         const int& t_col, 
+//                          cosnt int& row_dim,       const int& col_dim // column major
+//                          const int& wmma_row_dim,  const int& wmma_col_dim)
 //{
-////	for(int col = t_col*wmma_col_dim; col < (t_col+1)*wmma_col_dim; col++){
-//		memcpy( to+(col-t_col*wmma_col_dim)*wmma_row_dim, 
-//						from+col*row_dim+t_row*wmma_row_dim, 
-//						wmma_row_dim*sizeof(hf) );
-////	}
+////  for(int col = t_col*wmma_col_dim; col < (t_col+1)*wmma_col_dim; col++){
+//    memcpy( to+(col-t_col*wmma_col_dim)*wmma_row_dim, 
+//            from+col*row_dim+t_row*wmma_row_dim, 
+//            wmma_row_dim*sizeof(hf) );
+////  }
 //}
 
 // Performs an MxNxK GEMM (C=alpha*A*B + beta*C) assuming:
@@ -60,255 +60,255 @@ const int WMMA_K = 16;
 // Note: This is NOT a high performance example but is for demonstration purposes only
 //       For a high performance code please use the GEMM provided in cuBLAS.
 __global__ void wmma_example(half *a, half *b, float *c, int M, int N, int K, float alpha, float beta) {
-	
-// M, N, K are the block-wise matrix dim.
-	int tm_dim = M/WMMA_M; // tile dimension in m.
-	int tn_dim = N/WMMA_N;
-	int tk_dim = K/WMMA_K;
 
-// Declare all shared memory.
-	extern __shared__ float4 sm[];
+  // M, N, K are the block-wise matrix dim.
+  int tm_dim = M/WMMA_M; // tile dimension in m.
+  int tn_dim = N/WMMA_N;
+  int tk_dim = K/WMMA_K;
 
-	half* sm_a = (half*)sm;
-	half* sm_b = sm_a + M*K;
-	float* sm_c = (float*)(sm_b + K*N);
+  // Declare all shared memory.
+  extern __shared__ float4 sm[];
 
-	half* wmma_a = (half*)(sm_c + M*N);
-	half* wmma_b = wmma_a + WMMA_M*WMMA_K;
-	float* wmma_c = (float*)(wmma_b + WMMA_K*WMMA_N);
+  half* sm_a = (half*)sm;
+  half* sm_b = sm_a + M*K;
+  float* sm_c = (float*)(sm_b + K*N);
 
-// Copy stuff from global memory to shared memory.
-	int global_n = blockIdx.x*blockDim.x+threadIdx.x;
+  half* wmma_a = (half*)(sm_c + M*N);
+  half* wmma_b = wmma_a + WMMA_M*WMMA_K;
+  float* wmma_c = (float*)(wmma_b + WMMA_K*WMMA_N);
 
-	if(threadIdx.x == 0){
-		for(int k = 0; k < blockDim.y; k++){
-			sm_a[threadIdx.y*blockDim.y+k] = a[threadIdx.y*blockDim.y+k];
-		}
-	}
+  // Copy stuff from global memory to shared memory.
+  int global_n = blockIdx.x*blockDim.x+threadIdx.x;
 
-	__syncthreads();
+  if(threadIdx.x == 0){
+    for(int k = 0; k < blockDim.y; k++){
+      sm_a[threadIdx.y*blockDim.y+k] = a[threadIdx.y*blockDim.y+k];
+    }
+  }
 
-//	sm_b[threadIdx.y*blockDim.x+threadIdx.x] = b[threadIdx.y*blockDim.x*gridDim.x+global_n];
-	sm_b[threadIdx.x*blockDim.y+threadIdx.y] = b[global_n*blockDim.y+threadIdx.y];
-	
-	__syncthreads();
+  __syncthreads();
 
-//	sm_c[threadIdx.x*blockDim.y+threadIdx.y] = 0.;
-//	for(int k = 0; k < blockDim.y; k++){
-//		sm_c[threadIdx.x*blockDim.y+threadIdx.y] += float(sm_a[k*blockDim.y+threadIdx.y]*sm_b[threadIdx.x*blockDim.y+k]);
-//	}
-	
-	__syncthreads();
+  //  sm_b[threadIdx.y*blockDim.x+threadIdx.x] = b[threadIdx.y*blockDim.x*gridDim.x+global_n];
+  sm_b[threadIdx.x*blockDim.y+threadIdx.y] = b[global_n*blockDim.y+threadIdx.y];
 
-// Set up the wmma stuff
-	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> a_frag;
+  __syncthreads();
+
+  //  sm_c[threadIdx.x*blockDim.y+threadIdx.y] = 0.;
+  //  for(int k = 0; k < blockDim.y; k++){
+  //    sm_c[threadIdx.x*blockDim.y+threadIdx.y] += float(sm_a[k*blockDim.y+threadIdx.y]*sm_b[threadIdx.x*blockDim.y+k]);
+  //  }
+
+  __syncthreads();
+
+  // Set up the wmma stuff
+  wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> a_frag;
   wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b_frag;
   wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
-	for( int tm = 0; tm < tm_dim; tm++ ){
-	for( int tn = 0; tn < tn_dim; tn++ ){
-		// Initialize the output to zero
-  	wmma::fill_fragment(c_frag, 0.0f);
-		// for this particular tile, loop over k
-		for( int tk = 0; tk < tk_dim; tk++ ){
-			
-			// Copy (tm,tk) tile from sm_a to wmma_a
-			if( threadIdx.y >= tk*WMMA_K && threadIdx.y < (tk+1)*WMMA_K && threadIdx.x == 0 ){
-				memcpy( wmma_a+(threadIdx.y-tk*WMMA_K)*WMMA_M, sm_a+threadIdx.y*M+tm*WMMA_M, WMMA_M*sizeof(half) );
-			}
-			// Copy (tk,tn) tile from sm_b to wmma_b
-			if( threadIdx.x >= tn*WMMA_N && threadIdx.x < (tn+1)*WMMA_N && threadIdx.y == 0 ){
-				memcpy( wmma_b+(threadIdx.x-tn*WMMA_N)*WMMA_K, sm_b+threadIdx.x*K+tk*WMMA_K, WMMA_K*sizeof(half) );
-			}
-			__syncthreads();
-			
-			// Load the inputs
-		  wmma::load_matrix_sync(a_frag, wmma_a, WMMA_M);
-		  wmma::load_matrix_sync(b_frag, wmma_b, WMMA_K);
-			// Perform the matrix multiplication
-		  wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
-		}
-		// Store the output
-  	wmma::store_matrix_sync(wmma_c, c_frag, WMMA_N, wmma::mem_col_major);
-		__syncthreads();
-		// Copy (tm,tn) tile from wmma_c to sm_c
-		if( threadIdx.x >= tn*WMMA_N && threadIdx.x < (tn+1)*WMMA_N && threadIdx.y == 0 ){
-			memcpy( sm_c+threadIdx.x*M+tm*WMMA_M, wmma_c+(threadIdx.x-tn*WMMA_N)*WMMA_M, WMMA_M*sizeof(float) );
-		}
-		__syncthreads();
-	
-	}}
-	
-	__syncthreads();
+  for( int tm = 0; tm < tm_dim; tm++ ){
+  for( int tn = 0; tn < tn_dim; tn++ ){
+    // Initialize the output to zero
+    wmma::fill_fragment(c_frag, 0.0f);
+    // for this particular tile, loop over k
+    for( int tk = 0; tk < tk_dim; tk++ ){
 
-// Store result to global memory
-//	c[threadIdx.y*blockDim.x*gridDim.x+global_n] = sm_c[threadIdx.y*blockDim.x+threadIdx.x];
-	c[global_n*blockDim.y+threadIdx.y] = sm_c[threadIdx.x*blockDim.y+threadIdx.y];
+      // Copy (tm,tk) tile from sm_a to wmma_a
+      if( threadIdx.y >= tk*WMMA_K && threadIdx.y < (tk+1)*WMMA_K && threadIdx.x == 0 ){
+        memcpy( wmma_a+(threadIdx.y-tk*WMMA_K)*WMMA_M, sm_a+threadIdx.y*M+tm*WMMA_M, WMMA_M*sizeof(half) );
+      }
+      // Copy (tk,tn) tile from sm_b to wmma_b
+      if( threadIdx.x >= tn*WMMA_N && threadIdx.x < (tn+1)*WMMA_N && threadIdx.y == 0 ){
+        memcpy( wmma_b+(threadIdx.x-tn*WMMA_N)*WMMA_K, sm_b+threadIdx.x*K+tk*WMMA_K, WMMA_K*sizeof(half) );
+      }
+      __syncthreads();
+
+      // Load the inputs
+      wmma::load_matrix_sync(a_frag, wmma_a, WMMA_M);
+      wmma::load_matrix_sync(b_frag, wmma_b, WMMA_K);
+      // Perform the matrix multiplication
+      wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+    }
+    // Store the output
+    wmma::store_matrix_sync(wmma_c, c_frag, WMMA_N, wmma::mem_col_major);
+    __syncthreads();
+    // Copy (tm,tn) tile from wmma_c to sm_c
+    if( threadIdx.x >= tn*WMMA_N && threadIdx.x < (tn+1)*WMMA_N && threadIdx.y == 0 ){
+      memcpy( sm_c+threadIdx.x*M+tm*WMMA_M, wmma_c+(threadIdx.x-tn*WMMA_N)*WMMA_M, WMMA_M*sizeof(float) );
+    }
+    __syncthreads();
+
+  }}
+
+  __syncthreads();
+
+  // Store result to global memory
+  //  c[threadIdx.y*blockDim.x*gridDim.x+global_n] = sm_c[threadIdx.y*blockDim.x+threadIdx.x];
+  c[global_n*blockDim.y+threadIdx.y] = sm_c[threadIdx.x*blockDim.y+threadIdx.y];
 
 }
 
 __global__ void convertFp32ToFp16 (half *out, float *in, int n) {
-	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	if (idx < n) {
-		out[idx] = in[idx];
-	}
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+  if (idx < n) {
+    out[idx] = in[idx];
+  }
 }
 
 int main(int argc, char* argv[]) {
-	float *a_fp32;
-	float *b_fp32;
-	half *a_fp16;
-	half *b_fp16;
+  float *a_fp32;
+  float *b_fp32;
+  half *a_fp16;
+  half *b_fp16;
 
-	float *c;
-	float *c_cublas;
-	float *c_wmma;
+  float *c;
+  float *c_cublas;
+  float *c_wmma;
 
-	float *c_host_cublas;
-	float *c_host_wmma;
+  float *c_host_cublas;
+  float *c_host_wmma;
 
-	curandGenerator_t gen;
-	cublasHandle_t cublasHandle;
+  curandGenerator_t gen;
+  cublasHandle_t cublasHandle;
 
-	cudaEvent_t startWMMA;
-	cudaEvent_t stopWMMA;
+  cudaEvent_t startWMMA;
+  cudaEvent_t stopWMMA;
 
-	cudaEvent_t startcublas;
-	cudaEvent_t stopcublas;
+  cudaEvent_t startcublas;
+  cudaEvent_t stopcublas;
 
-	cudaErrCheck(cudaEventCreate(&startWMMA));
-	cudaErrCheck(cudaEventCreate(&stopWMMA));
+  cudaErrCheck(cudaEventCreate(&startWMMA));
+  cudaErrCheck(cudaEventCreate(&stopWMMA));
 
-	cudaErrCheck(cudaEventCreate(&startcublas));
-	cudaErrCheck(cudaEventCreate(&stopcublas));
-
-
-	cublasErrCheck(cublasCreate(&cublasHandle));
-
-	// Use tensor cores
-	cublasErrCheck(cublasSetMathMode(cublasHandle, CUBLAS_TENSOR_OP_MATH));
-
-	cudaErrCheck(cudaMalloc((void**)&a_fp32, MATRIX_M * MATRIX_K * sizeof(float)));
-	cudaErrCheck(cudaMalloc((void**)&b_fp32, MATRIX_K * MATRIX_N * sizeof(float)));
-	cudaErrCheck(cudaMalloc((void**)&a_fp16, MATRIX_M * MATRIX_K * sizeof(half)));
-	cudaErrCheck(cudaMalloc((void**)&b_fp16, MATRIX_K * MATRIX_N * sizeof(half)));
-
-	cudaErrCheck(cudaMalloc((void**)&c, MATRIX_M * MATRIX_N * sizeof(float)));
-	cudaErrCheck(cudaMalloc((void**)&c_cublas, MATRIX_M * MATRIX_N * sizeof(float)));
-	cudaErrCheck(cudaMalloc((void**)&c_wmma, MATRIX_M * MATRIX_N * sizeof(float)));
-
-	c_host_cublas = (float*)malloc(MATRIX_M * MATRIX_N * sizeof(float));
-	c_host_wmma = (float*)malloc(MATRIX_M * MATRIX_N * sizeof(float));
-
-	curandErrCheck(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
-	curandErrCheck(curandSetPseudoRandomGeneratorSeed(gen, 1337ULL));
-
-	curandErrCheck(curandGenerateUniform(gen, a_fp32, MATRIX_M * MATRIX_K));
-	curandErrCheck(curandGenerateUniform(gen, b_fp32, MATRIX_K * MATRIX_N));
-
-	// curand doesn't currently support fp16 so we generate in fp32 and convert to fp16.
-	convertFp32ToFp16 <<< (MATRIX_M * MATRIX_K + 255) / 256, 256 >>> (a_fp16, a_fp32, MATRIX_M * MATRIX_K);
-	convertFp32ToFp16 <<< (MATRIX_K * MATRIX_N + 255) / 256, 256 >>> (b_fp16, b_fp32, MATRIX_K * MATRIX_N);
-
-	curandErrCheck(curandGenerateUniform(gen, c, MATRIX_M * MATRIX_N));
-
-	curandErrCheck(curandDestroyGenerator(gen));
-
-	cudaErrCheck(cudaMemcpy(c_cublas, c, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToDevice));
-	cudaErrCheck(cudaMemcpy(c_wmma, c, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToDevice));
-
-	float alpha = 1.0f;
-	float beta = 0.0f;
+  cudaErrCheck(cudaEventCreate(&startcublas));
+  cudaErrCheck(cudaEventCreate(&stopcublas));
 
 
-	printf("\nM = %d, N = %d, K = %d. alpha = %f, beta = %f\n\n", MATRIX_M, MATRIX_N, MATRIX_K, alpha, beta);
+  cublasErrCheck(cublasCreate(&cublasHandle));
 
-	// First: using WMMA
-	dim3 gridDim;
-	dim3 blockDim;
+  // Use tensor cores
+  cublasErrCheck(cublasSetMathMode(cublasHandle, CUBLAS_TENSOR_OP_MATH));
 
-	// blockDim.x must be a multple of warpSize
-	// 128x4 means we have 16 warps and a block computes a 64x64 output tile
-	blockDim.x = 32;
-	blockDim.y = MATRIX_M;
+  cudaErrCheck(cudaMalloc((void**)&a_fp32, MATRIX_M * MATRIX_K * sizeof(float)));
+  cudaErrCheck(cudaMalloc((void**)&b_fp32, MATRIX_K * MATRIX_N * sizeof(float)));
+  cudaErrCheck(cudaMalloc((void**)&a_fp16, MATRIX_M * MATRIX_K * sizeof(half)));
+  cudaErrCheck(cudaMalloc((void**)&b_fp16, MATRIX_K * MATRIX_N * sizeof(half)));
 
-	gridDim.x = (MATRIX_N + blockDim.x-1) / blockDim.x;
-	gridDim.y = 1;
+  cudaErrCheck(cudaMalloc((void**)&c, MATRIX_M * MATRIX_N * sizeof(float)));
+  cudaErrCheck(cudaMalloc((void**)&c_cublas, MATRIX_M * MATRIX_N * sizeof(float)));
+  cudaErrCheck(cudaMalloc((void**)&c_wmma, MATRIX_M * MATRIX_N * sizeof(float)));
 
-	printf("Running with wmma...\n");
-	cudaErrCheck(cudaEventRecord(startWMMA));
-	int shared_memory_size = blockDim.y*blockDim.y*2+blockDim.y*blockDim.x*2+blockDim.y*blockDim.x*4;
-	wmma_example <<< gridDim, blockDim, shared_memory_size*2 >>> (a_fp16, b_fp16, c_wmma, blockDim.y, blockDim.x, blockDim.y, alpha, beta);
-	cudaErrCheck(cudaEventRecord(stopWMMA));
+  c_host_cublas = (float*)malloc(MATRIX_M * MATRIX_N * sizeof(float));
+  c_host_wmma = (float*)malloc(MATRIX_M * MATRIX_N * sizeof(float));
 
+  curandErrCheck(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
+  curandErrCheck(curandSetPseudoRandomGeneratorSeed(gen, 1337ULL));
 
+  curandErrCheck(curandGenerateUniform(gen, a_fp32, MATRIX_M * MATRIX_K));
+  curandErrCheck(curandGenerateUniform(gen, b_fp32, MATRIX_K * MATRIX_N));
 
-	// Now using cuBLAS
-	printf("Running with cuBLAS...\n");
-	cudaErrCheck(cudaEventRecord(startcublas));
-	cublasErrCheck(cublasGemmEx(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, 
-				MATRIX_M, MATRIX_N, MATRIX_K, 
-				&alpha,
-				a_fp16, CUDA_R_16F, MATRIX_M,
-				b_fp16, CUDA_R_16F, MATRIX_K,
-				&beta, 
-				c_cublas, CUDA_R_32F, MATRIX_M,
-				CUDA_R_32F, CUBLAS_GEMM_DFALT_TENSOR_OP));
-	cudaErrCheck(cudaEventRecord(stopcublas));
+  // curand doesn't currently support fp16 so we generate in fp32 and convert to fp16.
+  convertFp32ToFp16 <<< (MATRIX_M * MATRIX_K + 255) / 256, 256 >>> (a_fp16, a_fp32, MATRIX_M * MATRIX_K);
+  convertFp32ToFp16 <<< (MATRIX_K * MATRIX_N + 255) / 256, 256 >>> (b_fp16, b_fp32, MATRIX_K * MATRIX_N);
 
-	// Error checking
-	printf("\nChecking results...\n");
-	cudaErrCheck(cudaMemcpy(c_host_wmma, c_wmma, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost));
-	cudaErrCheck(cudaMemcpy(c_host_cublas, c_cublas, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost));
+  curandErrCheck(curandGenerateUniform(gen, c, MATRIX_M * MATRIX_N));
 
-	// 0.01% relative tolerance. 1e-5 absolute tolerance.
-	int errors = 0;
-	for (int i = 0; i < MATRIX_M * MATRIX_N; i++) {
-		float v1 = c_host_wmma[i];
-		float v2 = c_host_cublas[i];
-		if (v1 / v2 > 1.0001 || v2 / v1 > 1.0001 || abs(v1 - v2) > 1e-5) {
-			errors++;
-			if (errors < 0) printf("%06d %f %f\n", i, v1, v2);
-		}
-	}
+  curandErrCheck(curandDestroyGenerator(gen));
 
-	if (errors < 0) {
-		printf("WMMA does not agree with cuBLAS! %d errors!\n", errors);
-	}
-	else {
-		printf("Results verified: cublas and WMMA agree.\n\n");
-		float wmmaTime;
-		float cublasTime;
-		cudaErrCheck(cudaEventSynchronize(stopWMMA));
-		cudaErrCheck(cudaEventSynchronize(stopcublas));
-		cudaErrCheck(cudaEventElapsedTime(&wmmaTime, startWMMA, stopWMMA));
-		cudaErrCheck(cudaEventElapsedTime(&cublasTime, startcublas, stopcublas));
-		printf("wmma took %fms\n", wmmaTime);
-		printf("cublas took %fms\n", cublasTime);
+  cudaErrCheck(cudaMemcpy(c_cublas, c, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToDevice));
+  cudaErrCheck(cudaMemcpy(c_wmma, c, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToDevice));
 
-		printf("\nFor a faster code using wmma you should check out the cudaTensorCoreGemm sample in the CUDA Toolkit.\nThis code was written as a demo only!\n\n");
-	}
+  float alpha = 1.0f;
+  float beta = 0.0f;
 
 
-	cudaErrCheck(cudaEventDestroy(startWMMA));
-	cudaErrCheck(cudaEventDestroy(stopWMMA));
+  printf("\nM = %d, N = %d, K = %d. alpha = %f, beta = %f\n\n", MATRIX_M, MATRIX_N, MATRIX_K, alpha, beta);
 
-	cudaErrCheck(cudaEventDestroy(startcublas));             
-	cudaErrCheck(cudaEventDestroy(stopcublas));
+  // First: using WMMA
+  dim3 gridDim;
+  dim3 blockDim;
 
-	cudaErrCheck(cudaFree(a_fp32));
-	cudaErrCheck(cudaFree(b_fp32));
-	cudaErrCheck(cudaFree(a_fp16));
-	cudaErrCheck(cudaFree(b_fp16));
+  // blockDim.x must be a multple of warpSize
+  // 128x4 means we have 16 warps and a block computes a 64x64 output tile
+  blockDim.x = 32;
+  blockDim.y = MATRIX_M;
 
-	cudaErrCheck(cudaFree(c));
-	cudaErrCheck(cudaFree(c_cublas));
-	cudaErrCheck(cudaFree(c_wmma));
+  gridDim.x = (MATRIX_N + blockDim.x-1) / blockDim.x;
+  gridDim.y = 1;
 
-	free(c_host_cublas);
-	free(c_host_wmma);
+  printf("Running with wmma...\n");
+  cudaErrCheck(cudaEventRecord(startWMMA));
+  int shared_memory_size = blockDim.y*blockDim.y*2+blockDim.y*blockDim.x*2+blockDim.y*blockDim.x*4;
+  wmma_example <<< gridDim, blockDim, shared_memory_size*2 >>> (a_fp16, b_fp16, c_wmma, blockDim.y, blockDim.x, blockDim.y, alpha, beta);
+  cudaErrCheck(cudaEventRecord(stopWMMA));
 
-	cudaErrCheck(cudaDeviceReset());
-	return 0;
+
+
+  // Now using cuBLAS
+  printf("Running with cuBLAS...\n");
+  cudaErrCheck(cudaEventRecord(startcublas));
+  cublasErrCheck(cublasGemmEx(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, 
+        MATRIX_M, MATRIX_N, MATRIX_K, 
+        &alpha,
+        a_fp16, CUDA_R_16F, MATRIX_M,
+        b_fp16, CUDA_R_16F, MATRIX_K,
+        &beta, 
+        c_cublas, CUDA_R_32F, MATRIX_M,
+        CUDA_R_32F, CUBLAS_GEMM_DFALT_TENSOR_OP));
+  cudaErrCheck(cudaEventRecord(stopcublas));
+
+  // Error checking
+  printf("\nChecking results...\n");
+  cudaErrCheck(cudaMemcpy(c_host_wmma, c_wmma, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost));
+  cudaErrCheck(cudaMemcpy(c_host_cublas, c_cublas, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost));
+
+  // 0.01% relative tolerance. 1e-5 absolute tolerance.
+  int errors = 0;
+  for (int i = 0; i < MATRIX_M * MATRIX_N; i++) {
+    float v1 = c_host_wmma[i];
+    float v2 = c_host_cublas[i];
+    if (v1 / v2 > 1.0001 || v2 / v1 > 1.0001 || abs(v1 - v2) > 1e-5) {
+      errors++;
+      if (errors < 0) printf("%06d %f %f\n", i, v1, v2);
+    }
+  }
+
+  if (errors < 0) {
+    printf("WMMA does not agree with cuBLAS! %d errors!\n", errors);
+  }
+  else {
+    printf("Results verified: cublas and WMMA agree.\n\n");
+    float wmmaTime;
+    float cublasTime;
+    cudaErrCheck(cudaEventSynchronize(stopWMMA));
+    cudaErrCheck(cudaEventSynchronize(stopcublas));
+    cudaErrCheck(cudaEventElapsedTime(&wmmaTime, startWMMA, stopWMMA));
+    cudaErrCheck(cudaEventElapsedTime(&cublasTime, startcublas, stopcublas));
+    printf("wmma took %fms\n", wmmaTime);
+    printf("cublas took %fms\n", cublasTime);
+
+    printf("\nFor a faster code using wmma you should check out the cudaTensorCoreGemm sample in the CUDA Toolkit.\nThis code was written as a demo only!\n\n");
+  }
+
+
+  cudaErrCheck(cudaEventDestroy(startWMMA));
+  cudaErrCheck(cudaEventDestroy(stopWMMA));
+
+  cudaErrCheck(cudaEventDestroy(startcublas));             
+  cudaErrCheck(cudaEventDestroy(stopcublas));
+
+  cudaErrCheck(cudaFree(a_fp32));
+  cudaErrCheck(cudaFree(b_fp32));
+  cudaErrCheck(cudaFree(a_fp16));
+  cudaErrCheck(cudaFree(b_fp16));
+
+  cudaErrCheck(cudaFree(c));
+  cudaErrCheck(cudaFree(c_cublas));
+  cudaErrCheck(cudaFree(c_wmma));
+
+  free(c_host_cublas);
+  free(c_host_wmma);
+
+  cudaErrCheck(cudaDeviceReset());
+  return 0;
 }
 
 
